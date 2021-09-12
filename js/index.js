17d19273bd44aaa1f40e8d1a6c22271f78e5c29a
MoaazAA <moaazabdelhamid1@gmail.com>
2021-09-12 18:43:00 +0200

    test 19

--- js/index.js
+++ js/index.js
@@ -22,20 +22,3 @@ $('#pic3').click(function(){
     $('.pic2').slideUp(500)
 })
 
-let per1 = document.querySelector('per1')
-let per2 = document.querySelector('per2')
-let per3 = document.querySelector('per3')
-let per4 = document.querySelector('per4')
-
-per1.onmouseemter = function(){
-    i.style.display =  ''
-}
-per2.onmouseemter = function(){
-    i.style.display =  ''
-}
-per3.onmouseemter = function(){
-    i.style.display =  ''
-}
-per4.onmouseemter = function(){
-    i.style.display =  ''
-}
